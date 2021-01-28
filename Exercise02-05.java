983f807ae83d1608ea7287c8dbf6d490ce9c1722
rasdinho <ras.dinho7@gmail.com>
2021-01-27 19:18:58 -0500

    first-quiz

---
+++ Exercise02-05.java
@@ -0,0 +1,36 @@
+// (Financial application: calculate tips)
+
+// Write a program that reads the subtotal and the gratuity rate, then computes the gratuity and total.
+
+// For example, if the user enters 10 for subtotal and 15% for gratuity rate,
+// the program displays $1.5 as gratuity and $11.5 as total.
+
+// ﻿Sample Run
+
+// Enter the subtotal and a gratuity rate: 10 15
+// The gratuity is $1.5 and total is $11.5
+
+// Class Name: Exercise02_05
+
+// If you get a logical or runtime error, please refer https://liveexample.pearsoncmg.com/faq.html.
+
+import java.util.Scanner;
+
+public class Exercise02_05 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);	// Create new Scanner object.
+
+		// Prompt the user to enter the subtotal and the gratuity rate
+		System.out.print("Enter the subtotal and a gratuity rate: ");
+		double subtotal = input.nextDouble();
+		double gratuityRate = input.nextDouble();
+
+		// Calculate gratuity and total
+		double gratuity = subtotal * (gratuityRate / 100);
+		double total = subtotal + gratuity;
+
+		// Display results
+		System.out.println("The gratuity is $" + gratuity +
+			" and total is $" + total);
+	}
+}
