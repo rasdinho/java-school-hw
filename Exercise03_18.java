3b5fd8de37a3b2f8bb28608e7a8cf3b24656b244
rasdinho <ras.dinho7@gmail.com>
2021-02-17 21:55:46 -0500

    Exercise04_18

--- Exercise03_18.java
+++ Exercise03_18.java
@@ -1,7 +1,8 @@
 // (Cost of shipping) A shipping company uses the following function to calculate the cost (in dollars) of shipping based on the weight of the package (in pounds).
 
 // c(w)=⎧⎩⎨⎪⎪⎪⎪3.5,if0<w<=15.5,if 1<w<=38.5,if 3<w<=1010.5,if 10<w<=20
-// Write a program that prompts the user to enter the weight of the package and displays the shipping cost. If the weight is negative or zero, display a message “Invalid input.” If the weight is greater than 20, display a message “The package cannot be shipped.”
+// Write a program that prompts the user to enter the weight of the package and displays the shipping cost. 
+// If the weight is negative or zero, display a message “Invalid input.” If the weight is greater than 20, display a message “The package cannot be shipped.”
 
 import java.util.Scanner;
 
