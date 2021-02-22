471c6e6fe8f3c64706c6f4614f3dd4891e7077eb
rasdinho <ras.dinho7@gmail.com>
2021-02-22 18:45:59 -0500

    loops

---
+++ loops_2.java
@@ -0,0 +1,9 @@
+// Given an int variable k that has already been declared, use a while loop to print a single line consisting of 88 asterisks. 
+// Use no variables other than k.
+
+
+k = 0;
+while (k < 88){
+   System.out.print("*");
+   k = k + 1;
+}
