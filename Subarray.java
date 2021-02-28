f32f67a7a7e3d4c663cbb1b9f844a7744f7911c7
rasdinho <ras.dinho7@gmail.com>
2021-02-28 00:14:05 -0500

    loops

---
+++ Subarray.java
@@ -0,0 +1,51 @@
+// Maximum Contiguous Subarray with Indices
+
+
+
+import java.util.Scanner;
+
+class MaximumSubarray {
+
+  private static int[] findMaxSubarrayIndices(int[] nums) {
+    int n = nums.length;
+    int maxSoFar = nums[0];
+    int maxEndingHere = nums[0];
+    int currentStart = 0, maxStart = 0, maxEnd = 0;
+
+    for(int i = 1; i < n; i++) {
+      if(maxEndingHere + nums[i] < nums[i]) {
+        maxEndingHere = nums[i];
+        currentStart = i;
+      } else {
+        maxEndingHere = maxEndingHere + nums[i];
+      }
+
+      if(maxEndingHere > maxSoFar) {
+        maxSoFar = maxEndingHere;
+        maxStart = currentStart;
+        maxEnd = i;
+      }
+    }
+
+    return new int[]{maxStart, maxEnd};
+  }
+
+  public static void main(String[] args) {
+    Scanner keyboard = new Scanner(System.in);
+    int n = keyboard.nextInt();
+    int[] nums = new int[n];
+    for(int i = 0; i < n; i++) {
+      nums[i] = keyboard.nextInt();
+    }
+
+    int[] indices = findMaxSubarrayIndices(nums);
+    int sum = 0;
+    System.out.print("Max contiguous subarray: ");
+    for(int i = indices[0]; i <= indices[1]; i++) {
+      System.out.print(nums[i] + " ");
+      sum += nums[i];
+    }
+    System.out.println();
+    System.out.println("Max contiguous subarray sum: " + sum);
+  }
+}
