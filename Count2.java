04d8249278024519ee1c72a2bcb8328e08e3c389
rasdinho <ras.dinho7@gmail.com>
2021-03-22 17:58:24 -0400

    count2

---
+++ Count2.java
@@ -0,0 +1,10 @@
+package problem1;
+
+
+public class Count2 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 4; i++) {
+            System.out.println("2 times " + i + " = " + 2 * i);
+        }
+    }
+}
